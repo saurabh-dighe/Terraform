3515063d385bd530d094a2a307e738b6ffae3747
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-25 22:24:03 +0530

    auto push

--- 02-variable/vars.tf
+++ 02-variable/vars.tf
@@ -2,6 +2,10 @@ variable "sample" {
     default = "This is sample comment"
 }
 
+output "vars-op" {
+    value = var.sample 
+}
+
 output "sample-op" {
     value = "Value of sample is ${var.sample}"  
 }
