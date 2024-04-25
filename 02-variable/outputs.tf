300a5fe3bbc9ec88b5d61ead5f09e4e122428301
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-26 00:00:31 +0530

    auto push

--- 02-variable/outputs.tf
+++ 02-variable/outputs.tf
@@ -1,6 +1,6 @@
-output "list-print" {
-    value = var.list-ex
-}
+# output "list-print" {
+#     value = var.list-ex
+# }
 
 output "list-ex-op" {
     value = "Name is ${var.list-ex[0]} and age is ${var.list-ex[1]}"
