82530c7df3fb49c4db85ab15f67996fbe4539364
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-27 13:30:36 +0530

    auto push

--- 05-security-grp/ec2.tf
+++ 05-security-grp/ec2.tf
@@ -1,7 +1,19 @@
+resource "aws_instance" "remote" {
+  ami           = "ami-072983368f2a6eab5"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tf-instance-remote"
+  }
+}
+
+output "tf-private-IP" {
+  value = aws_instance.remote.private_ip
+}
 resource "aws_security_group" "example" {
   name   = "sg"
   vpc_id = aws_vpc.example.id
 
   ingress = []
   egress  = []
-}
+}  
