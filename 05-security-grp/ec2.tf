f701897827d9958f57d8872036dc6748f0033562
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-27 14:37:02 +0530

    auto push

--- 05-security-grp/ec2.tf
+++ 05-security-grp/ec2.tf
@@ -12,7 +12,7 @@ output "tf-private-IP" {
 }
 resource "aws_security_group" "remote-sg" {
   name   = "remote-sg"
-  vpc_id = aws_vpc.remote-sg.id
+  vpc_id = "vpc-0b491c91c56da8a5b"
 
   ingress = []
   egress  = []
