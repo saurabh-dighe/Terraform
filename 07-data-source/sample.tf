18f99ade5fe220b2ace4c6b2320a097c436679b6
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-28 21:03:29 +0530

    auto push

---
+++ 07-data-source/sample.tf
@@ -0,0 +1,12 @@
+data "aws_instance" "foo" {
+#   instance_id = "i-instanceid"
+
+  filter {
+    name   = "tag:Name"
+    values = ["Jenkins"]
+  }
+}
+
+output "instance-ID" {
+  value = data.aws_instance.foo
+}
