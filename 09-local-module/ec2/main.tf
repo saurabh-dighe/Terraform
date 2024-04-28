07f2819b68358937539d4741ee3b9f56a6c68adf
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-29 00:16:40 +0530

    auto push

--- 09-local-module/ec2/main.tf
+++ 09-local-module/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "local-module" {
   ami           = "ami-072983368f2a6eab5"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   security_groups = [var.sg]
   tags = {
     Name = "tf-instance-dev"
