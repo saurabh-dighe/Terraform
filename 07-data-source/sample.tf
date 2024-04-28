317f527185aa9c35fca1dc968227079e741d863c
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-28 21:12:07 +0530

    auto push

--- 07-data-source/sample.tf
+++ 07-data-source/sample.tf
@@ -10,3 +10,13 @@ data "aws_instance" "foo" {
 output "instance-ID" {
   value = data.aws_instance.foo.id
 }
+
+data "aws_ami" "custom_ami" {
+  most_recent      = true
+  name_regex       = "^DevOps-LabImage-Centos$"
+  owners           = ["355449129696"]
+}
+
+output "custom_ami-ID" {
+  value = data.aws_ami.custom_ami.id
+}
