497aef53ee7a2c780041e48752b25c25fd172c89
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-29 00:06:31 +0530

    auto push

--- 09-local-module/ec2/main.tf
+++ 09-local-module/ec2/main.tf
@@ -1,15 +1,15 @@
-resource "aws_instance" "local" {
+resource "aws_instance" "local-module" {
   ami           = "ami-072983368f2a6eab5"
   instance_type = "t3.micro"
   security_groups = [var.sg]
-
+  depends_on = [sg-module]
   tags = {
     Name = "tf-instance-dev"
   }
 }
 
 output "tf-private-IP" {
-  value = aws_instance.local.private_ip
+  value = aws_instance.local-module.private_ip
 }
 
 variable sg {}
