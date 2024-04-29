74166b198c18b0a18d8818fa7878e0aa091b2a13
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-29 22:05:55 +0530

    auto push

--- 12-mapVariables/vars.tf
+++ 12-mapVariables/vars.tf
@@ -1,20 +1,19 @@
 variable "COMPONENTS" {
     default = {
         cart = {
-            instance_type = t3.micro
+            instance_type = "t3.micro"
         },
         catalouge = {
-            Name = "catalouge"
-            instance_type = t3.medium
+            instance_type = "t3.medium"
         },
         payment = {
-            instance_type = t3.large
+            instance_type = "t3.large"
         },
         shipping = {
-            instance_type = t2.micro
+            instance_type = "t2.micro"
         },
         user = {
-            instance_type = t3.micro
+            instance_type = "t3.micro"
         }
     }
 }
