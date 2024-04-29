ae6c42895027ce05f76b55ae6276ecf22059f702
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-29 22:08:12 +0530

    auto push

--- 12-mapVariables/ec2/ec2.tf
+++ 12-mapVariables/ec2/ec2.tf
@@ -4,7 +4,7 @@ data "aws_ami" "lab_ami" {
   owners           = ["355449129696"]
 }
 resource "aws_instance" "local-module" {
-  ami                        = data.aws_ami.lab_ami
+  ami                        = data.aws_ami.lab_ami.id
   instance_type              = var.component
   vpc_security_group_ids     = [var.sg]
 
