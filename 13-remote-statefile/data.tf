08e09d3f0ebf58b06fa81c1ccc90434b74883740
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-06 12:05:51 +0530

    auto pushhh

--- 13-remote-statefile/data.tf
+++ 13-remote-statefile/data.tf
@@ -1,8 +1,8 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "saurabh-bucket-tf"
-    key    = "dev/terraform-vpc/teraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# data "terraform_remote_state" "vpc" {
+#   backend = "s3"
+#   config = {
+#     bucket = "saurabh-bucket-tf"
+#     key    = "dev/terraform-vpc/teraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
