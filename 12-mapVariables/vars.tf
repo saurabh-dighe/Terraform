adfa1e87f30e83c252579e5f6066357809aa7d0d
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-09-09 12:10:11 +0530

    Auto push

--- 12-mapVariables/vars.tf
+++ 12-mapVariables/vars.tf
@@ -1,29 +1,29 @@
 variable "COMPONENTS" {
     default = {
         cart = {
-            instance_type = "t3.micro"
-            volume_size = 11 # in GB <<----- I increased this!
-            volume_type = "gp3"        
+            instance_type   = "t3.micro"
+            volume_size     = 11 # in GB <<----- I increased this!
+            volume_type     = "gp3"        
         },
         catalouge = {
-            instance_type = "t3.medium"
-            volume_size = 12 
-            volume_type = "gp3"             
+            instance_type   = "t3.medium"
+            volume_size     = 12 
+            volume_type     = "gp3"             
         },
         payment = {
-            instance_type = "t3.large"
-            volume_size = 13 
-            volume_type = "gp3" 
+            instance_type   = "t3.large"
+            volume_size     = 13 
+            volume_type     = "gp3" 
         },
         shipping = {
-            instance_type = "t2.micro"
-            volume_size = 14
-            volume_type = "gp3" 
+            instance_type   = "t2.micro"
+            volume_size     = 14
+            volume_type     = "gp3" 
         },
         user = {
-            instance_type = "t3.micro"
-            volume_size = 15 
-            volume_type = "gp3" 
+            instance_type   = "t3.micro"
+            volume_size     = 15 
+            volume_type     = "gp3" 
         }
     }
 }
